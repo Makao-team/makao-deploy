6eca20e95c43b51e81ad9e7112864bd3c4d1c648
dino.han <dino.han@daangn.com>
2025-06-01 21:41:51 +0900

    refac: 스키마 주석 추가

--- db/src/main/resources/db/schema.sql
+++ db/src/main/resources/db/schema.sql
@@ -1,21 +1,47 @@
--- project
+-- 프로젝트
 CREATE TABLE project
 (
     id                 BIGSERIAL PRIMARY KEY,
+
+    /**
+     * 프로젝트명
+     */
     name               TEXT,
+
+    /**
+     * 설명
+     */
     description        TEXT,
+
+    /**
+     * git 레포지토리
+     */
     git_repository_url TEXT,
+
     created_at         TIMESTAMP,
     is_archived        BOOLEAN DEFAULT FALSE
 );
 
--- admin user
+-- 관리자 계정(사용자용)
 CREATE TABLE admin_user
 (
     id          BIGSERIAL PRIMARY KEY,
+
+    /**
+     * 관리자 이름
+     */
     name        TEXT,
+
+    /**
+     * 관리자 이메일
+     */
     email       TEXT UNIQUE,
+
+    /**
+     * 비밀번호
+     */
     password    TEXT,
+
     created_at  TIMESTAMP,
     is_archived BOOLEAN DEFAULT FALSE
 );
