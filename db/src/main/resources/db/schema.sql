208b1c41e3ebf5d325f0bacfb41cb422658b7561
dino.han <dino.han@daangn.com>
2025-06-07 16:41:20 +0900

    fix: 코드리뷰반영

--- db/src/main/resources/db/schema.sql
+++ db/src/main/resources/db/schema.sql
@@ -3,19 +3,13 @@ CREATE TABLE project
 (
     id                 BIGSERIAL PRIMARY KEY,
 
-    /**
-     * 프로젝트명
-     */
+    -- 프로젝트명
     name               TEXT,
 
-    /**
-     * 설명
-     */
+    -- 설명
     description        TEXT,
 
-    /**
-     * git 레포지토리
-     */
+    -- git 레포지토리
     git_repository_url TEXT,
 
     created_at         TIMESTAMP,
@@ -27,19 +21,13 @@ CREATE TABLE admin_user
 (
     id          BIGSERIAL PRIMARY KEY,
 
-    /**
-     * 관리자 이름
-     */
+    -- 관리자 이름
     name        TEXT,
 
-    /**
-     * 관리자 이메일
-     */
+    -- 관리자 이메일
     email       TEXT UNIQUE,
 
-    /**
-     * 비밀번호
-     */
+    -- 비밀번호
     password    TEXT,
 
     created_at  TIMESTAMP,
