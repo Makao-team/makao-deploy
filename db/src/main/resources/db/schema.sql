402feff15e4276d155513070485e6201b25731d9
Jchan <gkswocks123@gmail.com>
2025-07-12 17:05:45 +0900

    refac: project table 삭제

--- db/src/main/resources/db/schema.sql
+++ db/src/main/resources/db/schema.sql
@@ -1,21 +1,3 @@
--- 프로젝트
-CREATE TABLE project
-(
-    id                 BIGSERIAL PRIMARY KEY,
-
-    -- 프로젝트명
-    name               TEXT,
-
-    -- 설명
-    description        TEXT,
-
-    -- git 레포지토리
-    git_repository_url TEXT,
-
-    created_at         TIMESTAMP,
-    is_archived        BOOLEAN DEFAULT FALSE
-);
-
 -- 관리자 계정(사용자용)
 CREATE TABLE admin_user
 (
